a013e2e0119cd46ac387420d51b16c60f1acab7b
HenriqueSylvestre <henriquesylvestresilva@gmail.com>
2025-02-06 00:07:02 -0300

    feat: edit logs consumer e producer sqs

--- src/main/java/com/sylvestre/aws_sqs/consumers/ConsumerSQS.java
+++ src/main/java/com/sylvestre/aws_sqs/consumers/ConsumerSQS.java
@@ -3,6 +3,7 @@ package com.sylvestre.aws_sqs.consumers;
 import com.sylvestre.aws_sqs.dtos.MyMessage;
 import io.awspring.cloud.sqs.annotation.SqsListener;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 
@@ -10,10 +11,13 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class ConsumerSQS {
 
+    @Value("${aws.sqs.nome-fila}")
+    private String queueName;
+
     @SqsListener("${aws.sqs.nome-fila}")
     public void listen(MyMessage message) {
         try {
-            log.info("Message: {}", message );
+            log.info("Consumindo mensagem da fila SQS - {}. Message: {}", queueName, message );
         } catch (Exception exception) {
             log.error("Erro ao consumir mensagem SQS.", exception);
         }
--- src/main/java/com/sylvestre/aws_sqs/producers/ProducerSQS.java
+++ src/main/java/com/sylvestre/aws_sqs/producers/ProducerSQS.java
@@ -3,11 +3,13 @@ package com.sylvestre.aws_sqs.producers;
 import com.sylvestre.aws_sqs.dtos.MyMessage;
 import io.awspring.cloud.sqs.operations.SqsTemplate;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class ProducerSQS {
 
     @Value("${aws.sqs.nome-fila}")
@@ -17,6 +19,7 @@ public class ProducerSQS {
 
     public void producerMessage(MyMessage message) {
 
+        log.info("Publicando mensagem na fila SQS - {}. Mensagem: {}",queueName, message);
         sqsTemplate.send(sqsSendOptions -> sqsSendOptions
                 .queue(queueName)
                 .payload(message));
